50f65de6d1a39f7fde4e1e3948f5a2d608dc34f3
Tom Koele <tom@codeandcoding.com>
2019-01-16 22:46:31 +0100

    added form render logic

--- src/Form.vue
+++ src/Form.vue
@@ -1,14 +1,92 @@
 <template>
     <form>
         Super form.
+        <component 
+            v-for="field in fields" 
+            :key="field.key"
+            :is="field.component"
+            v-bind="field.props">
+        </component>
     </form>
 </template>
 
 <script>
     import Vue from 'vue';
+    import * as inputs from './';
 
     export default {
         name: 'SuperForm',
+        props: {
+            schema: {
+                type: Object,
+                required: true,
+            },
+        },
+        computed: {
+            fields() {
+                const fields = this.schema.properties;
+                return Object.keys(fields).map(field => this.getFieldConfig(field, fields[field]));
+            }
+        },
+        methods: {
+            getFieldConfig(name, config) {
+                return {
+                    key: name,
+                    component: this.getFieldComponent(config),
+                    props: {
+                        name,
+                        ...this.getFieldProps(config),
+                    },
+                };
+            },
+            getFieldComponent(config) {
+                const widget = config['ui:widget'];
+                const hasKey = key => Object.prototype.hasOwnProperty.call(config, key);
+
+                let componentName;
+                switch (config.type) {
+                    case "number":
+                        componentName = hasKey('enum') ? 'SuperSelect' : 'SuperNumber';
+                        componentName = widget ? this.getWidgetComponent(widget) : componentName;
+                        break;
+                    case "string":
+                        componentName = hasKey('enum') ? 'SuperSelect' : 'SuperText';
+                        componentName = widget ? this.getWidgetComponent(widget) : componentName;
+                        break;
+                    case "array":
+                        componentName = widget ? this.getWidgetComponent(widget) : 'SuperSelect';
+                        break;
+                    case "null":
+                    case "boolean":
+                    case "object":
+                    case "integer":
+                    default:
+                        componentName = 'SuperText';
+                }
+
+                if (!inputs[componentName]) {
+                    console.warn(`component '${componentName}' not available`)
+                    componentName = 'SuperText';
+                }
+
+                return inputs[componentName];
+            },
+            getWidgetComponent(widget) {
+                switch (widget) {
+                    case 'radio':
+                        return 'SuperRadio';
+                    case 'checkbox':
+                        return 'SuperCheckbox';
+                    default:
+                        return 'SuperText';
+                }
+            },
+            getFieldProps(config) {
+                return {
+                    
+                };
+            }
+        },
     }
 </script>
 
