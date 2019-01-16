1bfcaa969fdd5526bb67c9735f4a0c6f4d4ce78c
MarcoLohmeijer <32522269+MarcoLohmeijer@users.noreply.github.com>
2019-01-16 17:19:47 +0100

    feat: implement text input and props with validation

--- src/inputs/Text.vue
+++ src/inputs/Text.vue
@@ -1,6 +1,12 @@
 <template>
-    <div class="input">
-        Input
+    <div class="text">
+        <label>{{ label }}</label>
+        <input :type="type" 
+        v-model="superText"
+        :id="id" 
+        :placeholder="placeholder"
+        :name="name" 
+        :value="value"/> 
     </div>
 </template>
 
@@ -9,6 +15,38 @@
 
     export default {
         name: 'SuperText',
+        data() {
+            return {
+                superText,
+            };
+        },
+        props: {
+            placeholder: { 
+                String,
+            },
+            label: {
+                String,
+                required: false
+            },
+            name: {
+                String,
+                required: false
+            },
+            id: {
+                String,
+                required: true
+            }, 
+            value: {
+                String,
+                required: false
+            },
+            type: {
+                validator: function (value) {
+                    // The value must match one of these strings
+                    return ['text', 'password'].indexOf(value) !== -1
+                }
+            }
+        },
     }
 </script>
 
