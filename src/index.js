724153189ed67b7fce6931b0e232ff052d1eceb4
Sheril Elizabeth Jose <sheril@Sherils-MacBook-Pro.local>
2019-01-17 16:13:52 +0100

    feat: Creating a rediobutton componet

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import SuperForm from './Form';
 import SuperText from './inputs/Text';
 import SuperSelect from './inputs/Select';
 import SuperCheckbox from './inputs/Checkbox';
+import SuperRadio from './Inputs/Radio';
 
 export {
     SuperText,
     SuperSelect,
     SuperCheckbox,
+    SuperRadio,
 };
 
 export default SuperForm;
---
+++ src/inputs/Radio.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <label v-for="item in selectItems" :key="item.value">
+            <input
+                type="radio"
+                :name="item.label"
+                v-on:input="e => onChange(item.value)" />
+            <span>{{ item.label }}</span>
+        </label>
+    </div>
+</template>
+
+<script>
+    import Vue from 'vue';
+    import _ from 'lodash';
+    import inputProps from '../inputProps';
+
+    export default {
+        name: 'SuperRadio',
+
+        props: _.assign({
+            items: {
+                type: Array,
+                required: false,
+            },
+        }, inputProps),
+
+        computed: {
+            selectItems() {               
+                return this.items.map(item => ({
+                    value: item,
+                    label: item,
+                }));
+            }
+        },
+
+        methods: {
+            onChange(value) {
+                this.$emit('onChange', this.name, value);
+            }
+        }
+    }
+</script>
+
+<style lang="scss">
+</style>
